b00eab8fe8ff3e2ded01b0abfff128ddc8fda246
Elliot Kantor <elliotjkantor@gmail.com>
2026-02-16 00:38:42 -0500

    Use uv in run scripts and add control center run script

--- simple_gridlabd_example/run_example_cc.sh
+++ simple_gridlabd_example/run_example_cc.sh
@@ -5,19 +5,25 @@ TIMESTAMP=$(date +%s)
 
 BROKER_LOG="./results/broker_$TIMESTAMP.log"
 TRANSMISSION_LOG="./results/TransmissionSim_$TIMESTAMP.log"
+CC_LOG="./results/CCSim_$TIMESTAMP.log"
 DISTRIBUTION_LOG="./results/DistributionSim_$TIMESTAMP.log"
 
 touch $BROKER_LOG
 touch $TRANSMISSION_LOG
 touch $DISTRIBUTION_LOG
+touch $CC_LOG
 
 uv sync
 
 HELICS_BROKER=`which helics_broker`
-($HELICS_BROKER -t="zmq" --federates=4 --name=mainbroker > $BROKER_LOG)&
+($HELICS_BROKER -t="zmq" --federates=5 --name=mainbroker > $BROKER_LOG)&
 
 cd Transmission
 uv run Transmission_simulator.py > ../$TRANSMISSION_LOG 2>&1 &
+cd ..
+
+cd CC
+uv run CC_simulator.py > ../$CC_LOG 2>&1 &
 cd ..
 
 cd Distribution
