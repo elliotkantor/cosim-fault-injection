8a599967daf65cd5f58650d6ffa150394ea2ab3e
Sabina <ssokol3@gatech.edu>
2026-02-17 21:24:42 +0000

    fault is injected into distribution federate with reliability module, relay is tripped, control center federate detects trip, publishes message to ros2

--- simple_gridlabd_example/cc_run_example.sh
+++ simple_gridlabd_example/cc_run_example.sh
@@ -7,11 +7,13 @@ BROKER_LOG="./results/broker_$TIMESTAMP.log"
 TRANSMISSION_LOG="./results/TransmissionSim_$TIMESTAMP.log"
 CC_LOG="./results/CCSim_$TIMESTAMP.log"
 DISTRIBUTION_LOG="./results/DistributionSim_$TIMESTAMP.log"
+RELAY_LOG="./results/RelaySim_$TIMESTAMP.log"
 
 touch $BROKER_LOG
 touch $TRANSMISSION_LOG
 touch $DISTRIBUTION_LOG
 touch $CC_LOG
+touch $RELAY_LOG
 
 uv sync
 
@@ -31,5 +33,5 @@ gridlabd IEEE_123_feeder_0.glm > ../$DISTRIBUTION_LOG 2>&1 &
 cd ..
 
 cd Relay 
-(exec uv run Relay_simulator.py)
+uv run Relay_simulator.py > ../$RELAY_LOG 2>&1 &
 cd ..
